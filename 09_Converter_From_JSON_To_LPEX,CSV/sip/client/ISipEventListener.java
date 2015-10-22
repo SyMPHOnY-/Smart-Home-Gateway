b404fb0e4149fca6536e44c9cb7b3067f3ad45d4
xzeman43 <xzeman43@gmail.com>
2015-10-22 17:00:29 +0300

    New version
    Updating to version 2.0

--- 09_Converter_From_JSON_To_LPEX,CSV/sip/client/ISipEventListener.java
+++
@@ -1,7 +0,0 @@
-package cz.vutbr.wislab.symphony.sip.client;
-
-import java.util.EventListener;
-
-public interface ISipEventListener extends EventListener {
-	public void onSipMessage(SipEvent sipEvent);
-}
--- 09_Converter_From_JSON_To_LPEX,CSV/sip/client/SipManagerInterface.java
+++
@@ -1,6 +0,0 @@
-package cz.vutbr.wislab.symphony.sip.client;
-
-public interface SipManagerInterface {
-	public void sendMessage(String to, String message) throws NotInitializedException;
-
-}
